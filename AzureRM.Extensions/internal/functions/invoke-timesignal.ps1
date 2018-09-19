e17bcc7774523feab5637f7aebb5a3046e97c391
Mötz Jensen <Motz@e-s.local>
2018-09-19 22:40:44 +0200

    Round 5 fixing issues for CI build process.

--- AzureRM.Extensions/internal/functions/invoke-timesignal.ps1
+++ AzureRM.Extensions/internal/functions/invoke-timesignal.ps1
@@ -30,10 +30,10 @@ Author: Mötz Jensen (@splaxi)
 function Invoke-TimeSignal {
     [CmdletBinding(DefaultParameterSetName = 'Start')]
     param (
-        [Parameter(Mandatory = $true, ParameterSetName = 'Start', Position = 1 )]                    
-        [switch] $Start,    
+        [Parameter(Mandatory = $true, ParameterSetName = 'Start', Position = 1 )]
+        [switch] $Start,
         
-        [Parameter(Mandatory = $True, ParameterSetName = 'End', Position = 2 )]                    
+        [Parameter(Mandatory = $True, ParameterSetName = 'End', Position = 2 )]
         [switch] $End
     )
     $Time = (Get-Date)
@@ -42,7 +42,7 @@ function Invoke-TimeSignal {
 
     if($Start.IsPresent) {
         if($Script:TimeSignals.ContainsKey($Command)) {
-            Write-PSFMessage -Level Verbose -Message "The command '$Command' is already taking part in time measurement."        
+            Write-PSFMessage -Level Verbose -Message "The command '$Command' is already taking part in time measurement."
         }
         else{
             $Script:TimeSignals.Add($Command, $Time)
@@ -56,7 +56,7 @@ function Invoke-TimeSignal {
             $Script:TimeSignals.Remove($Command)
         }
         else {
-            Write-PSFMessage -Level Verbose -Message "The command '$Command' was never started to take part in time measurement."        
+            Write-PSFMessage -Level Verbose -Message "The command '$Command' was never started to take part in time measurement."
         }
     }
 }
